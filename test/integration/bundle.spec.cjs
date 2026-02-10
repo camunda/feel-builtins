521d57f1b88b4ec81cb00207ef1e7bb1ede22577
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-02-10 22:58:10 +0100

    chore: simplify bundle test

--- test/integration/bundle.spec.cjs
+++ test/integration/bundle.spec.cjs
@@ -1,11 +1,11 @@
 const { camundaBuiltins } = require('@camunda/feel-builtins');
 
+const { expect } = require('chai');
+
 
 describe('integration - bundle', function() {
 
   it('should export CJS export', async function() {
-
-    const { expect } = await import('chai');
 
     // then
     expect(camundaBuiltins).not.to.be.empty;
